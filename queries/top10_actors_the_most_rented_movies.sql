e8be6505d8f2537f5a32f940b2b3c900b163bf3a
danikCosiachniy <danilashumkevich@gmail.com>
2025-10-30 12:10:38 +0300

    make corrections for the second query

--- queries/top10_actors_the_most_rented_movies.sql
+++ queries/top10_actors_the_most_rented_movies.sql
@@ -1,13 +1,11 @@
 SELECT
-    actor.actor_id,
-    CONCAT(actor.last_name, ' ', actor.first_name) AS actor_name,
-    COUNT(*) AS kol
-FROM actor
-    JOIN film_actor ON actor.actor_id = film_actor.actor_id
-    JOIN film f ON film_actor.film_id = f.film_id
-    JOIN inventory i ON f.film_id = i.film_id
-    JOIN rental r ON i.inventory_id = r.inventory_id
-WHERE r.return_date IS NOT NULL
-GROUP BY actor.actor_id, actor_name
-ORDER BY kol DESC
+  a.actor_id,
+  concat(a.last_name, ' ', a.first_name) AS actor_name,
+  COUNT(*) AS amount
+FROM actor AS a
+JOIN film_actor AS fa ON a.actor_id = fa.actor_id
+JOIN inventory   AS i  ON fa.film_id = i.film_id
+JOIN rental      AS r  ON i.inventory_id = r.inventory_id
+GROUP BY a.actor_id, actor_name
+ORDER BY amount DESC, actor_name
 LIMIT 10;
