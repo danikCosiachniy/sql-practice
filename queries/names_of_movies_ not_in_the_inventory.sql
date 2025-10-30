9c250fceb1c5f4fae8bec35eb8fb167893a74b19
danikCosiachniy <danilashumkevich@gmail.com>
2025-10-30 12:27:34 +0300

    make the last correction

--- queries/names_of_movies_ not_in_the_inventory.sql
+++ queries/names_of_movies_ not_in_the_inventory.sql
@@ -1,3 +1,7 @@
-SELECT film.film_id, film.title FROM
- film LEFT JOIN inventory ON film.film_id = inventory.film_id
-WHERE inventory.film_id IS NULL;
+SELECT f.film_id, f.title
+FROM film AS f
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM inventory AS i
+    WHERE i.film_id = f.film_id
+);
