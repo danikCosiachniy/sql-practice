dd4cf9ae5835d2c8e6a9369688946a853cd329b4
danikCosiachniy <danilashumkevich@gmail.com>
2025-10-30 12:21:10 +0300

    make corrections for the fifth query

--- queries/the_most_expensive_category.sql
+++ queries/the_most_expensive_category.sql
@@ -1,11 +1,10 @@
-SELECT category.category_id, category.name, sum(payment.amount) AS price
-FROM category
-JOIN film_category ON category.category_id = film_category.category_id
-JOIN film ON film_category.film_id = film.film_id
-JOIN inventory ON film.film_id = inventory.film_id
-JOIN rental ON inventory.inventory_id = rental.inventory_id
-JOIN payment ON rental.rental_id = payment.rental_id
-WHERE payment.amount > 0
-GROUP BY category.category_id, category.name
+SELECT c.category_id, c.name, sum(p.amount) AS price
+FROM category as c
+JOIN film_category as fc ON c.category_id = fc.category_id
+JOIN inventory as i ON fc.film_id = i.film_id
+JOIN rental as r ON i.inventory_id = r.inventory_id
+JOIN payment as p ON r.rental_id = p.rental_id
+WHERE p.amount > 0
+GROUP BY c.category_id, c.name
 ORDER BY price DESC
 LIMIT 1;
